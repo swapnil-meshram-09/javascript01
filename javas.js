4755c6240f67460d646ba65c01f73e90b18c7813
swapnil-meshram-09 <swapnilmeshram514@gmail.com>
2025-12-14 18:25:46 +0530

    first commit

--- javas.js
+++ javas.js
@@ -12,25 +12,3 @@ a = 1;              // reinit
 b = 2;
 // c = 3;
 
-
-check();
-
-function check(){
-    var a = 11;
-    let b = 12;
-    const c = 13; 
-
-    console.log("Inside Function");
-    
-    console.log(a);
-    console.log(b);
-    console.log(c);    
-
-}
-
-
-console.log("Outside Function");
-    
-console.log(a);
-console.log(b);
-console.log(c);   
